87dee73525605f38ff0ecb6a4c94f39601b977a2
Julio Caballero <173180@ids.upchiapas.edu.mx>
2019-03-07 10:47:29 -0600

    actividades

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+import I2C_LCD_driver
+from time import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+mylcd = I2C_LCD_driver.lcd()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('mensaje')
+def handleMessage(msg):
+    print('mensaje: ' + msg)
+    mylcd.lcd_display_string("                ", 1)
+    mylcd.lcd_display_string(msg, 1)
+
+if __name__ == '__main__':
+    #socketio.run(app,debug=True)
+    socketio.run(app,host='192.168.137.77',port=3000,debug=True)
